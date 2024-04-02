e0979595d8321024e5dce790806ea4a62fef093f
bsilawko <01187281@pw.edu.pl>
2024-04-02 21:56:55 +0200

    chnage Entry class name to entry

--- entry.h
+++ entry.h
@@ -1,20 +1,20 @@
 #pragma once
-#include <string>
 #include <iostream>
+#include <string>
 
-class Entry {
+class entry {
    private:
     std::string value;
     int count = 0;
 
    public:
-    Entry() = default;
-    Entry(std::string const& word, int count);
+    entry() = default;
+    entry(std::string const& word, int count);
 
     void operator++(int);
     std::string const& operator*() const;
     operator int() const;
 };
 
-std::ostream& operator<<(std::ostream& os, Entry const& entry);
-std::istream& operator>>(std::istream& is, Entry& entry);
+std::ostream& operator<<(std::ostream& os, entry const& entry);
+std::istream& operator>>(std::istream& is, entry& entry);
--- testGoogle.cpp
+++ testGoogle.cpp
@@ -3,39 +3,39 @@
 #include <sstream>
 
 #include "entry.h"
-TEST(EntryTest, default_ctor) {
-    Entry entry;
+TEST(entry_test, default_ctor) {
+    entry entry;
     ASSERT_EQ(*entry, "");
     ASSERT_EQ(int(entry), 0);
 }
 
-TEST(EntryTest, custom_ctor) {
-    Entry entry("żabka", 5);
+TEST(entry_test, custom_ctor) {
+    entry entry("żabka", 5);
     ASSERT_EQ(*entry, "żabka");
     ASSERT_EQ(int(entry), 5);
 }
 
-TEST(EntryTest, custom_ctor_negative_count) {
-    ASSERT_THROW(Entry("żabka", -1), std::exception);
+TEST(entry_test, custom_ctor_negative_count) {
+    ASSERT_THROW(entry("żabka", -1), std::exception);
 }
 
-TEST(EntryTest, operator_pp) {
-    Entry entry("żabka", 5);
+TEST(entry_test, operator_pp) {
+    entry entry("żabka", 5);
     ASSERT_EQ(int(entry), 5);
     entry++;
     ASSERT_EQ(int(entry), 6);
 }
 
-TEST(EntryTest, operator_out) {
-    Entry entry("żabka", 5);
+TEST(entry_test, operator_out) {
+    entry entry("żabka", 5);
     std::stringstream ss;
     ss << entry;
     ASSERT_EQ(ss.str(), "[żabka 5]");
 }
 
-TEST(EntryTest, operator_in) {
+TEST(entry_test, operator_in) {
     std::stringstream ss("[żabka 5]");
-    Entry entry;
+    entry entry;
     ss >> entry;
     ASSERT_EQ(*entry, "żabka");
     ASSERT_EQ(int(entry), 5);
